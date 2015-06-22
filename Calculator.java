e5bcb334c068bab7ea164c7dcccb5ab6afae335a
CaptainBonzo <lhopnetla@gmail.com>
2015-06-22 19:28:56 +0200

    userinput added

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class Calculator{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Bitte eingeben: [+-*/] zahl1 zahl2: ");
+		String operator = scan.next();
+		int zahl1= scan.nextInt();
+		int zahl2= scan.nextInt();
 	}
 }
